599af26c8cfe2cfe821aef7e0476cb9179ae0a79
Ricky MM <ricky.mafra@gmail.com>
2024-12-28 08:25:06 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ricky Mafra Moino on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
